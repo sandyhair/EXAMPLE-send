4681ac3c8c02673a814177729c4676a287c71bf5
sandyhair <86634848+sandyhair@users.noreply.github.com>
2022-01-19 17:31:23 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+camera = document.getElementById("camera");
+Webcam.attach('#camera'); 
+      
+  Webcam.set({
+    width:50,
+    height:300,
+    image_format :'png',
+    png_qualit:90
+  });
+
+function take_snapshot()
+{
+  Webcam.snap(function(data_uri)
+  {
+      document.getElementById("result").innerHTML = '<img id="captured_image src"'+data_uri+'"/>';
+  });
+}
+
+console.log('ml5 version', ml5.version);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/Dmr713jqH/model.json', modelLoaded);
+
+function modelLoaded()
+{
+    console.log("modelLoaded")
+}
